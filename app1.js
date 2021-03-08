1bb937daa8704763ceb1a8c2d271e2bff95898d9
Bhaskar Sen <bhaskarsen1998@gmail.com>
2021-03-08 19:05:25 +0530

    Loops

--- app1.js
+++ app1.js
@@ -107,5 +107,55 @@
   // || has higher precedence than ??, refer: 
   //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
   
+
+  // The operator ?? has a very low precedence, only a bit higher than ? and =, 
+  // so consider adding parentheses when using it in an expression.
+
+  // It’s forbidden to use it with || or && without explicit parentheses.
+
+
+//**loops **
+  
+  //loops are syntactically similar to cpp
+  //break and continue also work similar to proog conrol in cpp
+  //continue is used to decrease the levels of nesting caused by an if
+  //break or continue can't be used in ternary operator
+  
+  //labels(in genral, here used in loops)
+  //break can be used to manipulate program flow in general buy continue is only used in loops
+  //functions can be labelled as well(but not generator functions, ie. func* ())
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label
+  loop1:
+  for(let i = 0; i < 5; i++){
+    loop2:
+      for(let j = 0; j < 5; j++){
+          console.log(i,j);
+      if(j == 3)
+        break loop2;
+      if(i == 4)
+        break loop1;
+    }
+  }
+
+
+  //code produces op:
+  // 0 0 
+  // 0 1  
+  // 0 2 
+  // 0 3  
+  // 1 0  
+  // 1 1  
+  // 1 2  
+  // 1 3  
+  // 2 0  
+  // 2 1  
+  // 2 2  
+  // 2 3  
+  // 3 0  
+  // 3 1  
+  // 3 2  
+  // 3 3  
+  // 4 0
+
   
     
